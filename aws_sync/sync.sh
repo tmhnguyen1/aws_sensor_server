f1e3906ce7660fe23f279993db5121f1ed62a438
minhhoa-ng <minhhoaltv@gmail.com>
2024-01-01 20:48:02 +0800

    create adapted sync.sh

--- aws_sync/sync.sh
+++ aws_sync/sync.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "$(date) Sync entry" >> /home/ec2-user/learning/aws_ec2_sensor/cronlog.log
-aws s3 sync /home/ec2-user/learning/aws_ec2_sensor/data_csv/ s3://lalamove-apas/sensor_logger
+
+AWS_CLI_PATH="/usr/local/bin/aws"
+echo "$(date) Log sync entry" >> /app/data/synclog.log
+$AWS_CLI_PATH s3 sync /app/data/data_csv s3://lalamove-apas/counter/test_counter --region ap-east-1
