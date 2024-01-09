fbd4f831e96e646fa16a32046d3d6cc3c8424c92
Thi Minh Hoa NGUYEN <tmhnguyen@hkpc.org>
2024-01-09 15:27:29 +0800

    fix s3 path

--- aws_sync/sync.sh
+++ aws_sync/sync.sh
@@ -2,4 +2,4 @@
 
 AWS_CLI_PATH="/usr/local/bin/aws"
 echo "$(date) Log sync entry" >> /app/data/synclog.log
-$AWS_CLI_PATH s3 sync /app/data/data_csv s3://lalamove-apas/counter/test_counter --region ap-east-1
+$AWS_CLI_PATH s3 sync /app/data/data_csv s3://sengital-apas/sensor --region ap-east-1
